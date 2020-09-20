4ec08e45897cf7ae08dc6c00983e8bef9bb87e15
xianxianandkunkun <523068501@qq.com>
2020-09-20 11:38:02 +0800

    aaaaa

--- conftest.py
+++ conftest.py
@@ -3,6 +3,9 @@ import pytest
 from pages.login_page import LoginPage
 import os
 import allure
+from selenium.webdriver.chrome.options import Options
+import time
+import platform
 
 _driver = None
 
@@ -26,11 +29,23 @@ _driver = None
 #             allure.attach(_driver.get_screenshot_as_png(), "失败截图", allure.attachment_type.PNG)
 
 @pytest.fixture(scope="session")
-def driver():
+def driver(request):
     global _driver
     if _driver == None:
-        _driver = webdriver.Chrome()
-    _driver.maximize_window()
+        if platform.system() == 'Windows':
+            _driver = webdriver.Chrome()
+        else:
+            # Linux启动
+            chrome_options = Options()
+            chrome_options.add_argument('--window-size=1920,1080')
+            chrome_options.add_argument('--no-sandbox') #解决DevToolsActivePort文件不存在报错问题
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--disable-gpu') # 禁用GPU硬件加速
+            chrome_options.add_argument('--headless') # 无界面
+            #启动浏览器
+            _driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    _driver.maximize_window()  # 最大化
 
     yield _driver
 
